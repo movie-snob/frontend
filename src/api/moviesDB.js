f9dcca9b24b2bf56077730977fc94e34787e2d1f
Maxim Filippov <colixer@gmail.com>
2019-07-26 18:51:45 +0300

    API_KEY from env

--- src/api/moviesDB.js
+++ src/api/moviesDB.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const searchMovie = async (query) => {
-  const api_key = '49027f1d5d38f0459abe59fe5ab521ee'
+  const api_key = process.env.VUE_APP_MOVIEDB_AP_KEY
 
   return axios.get(`https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${api_key}`)
 }
