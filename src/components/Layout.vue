479347c09c92152f326e48562c9d25df35a8c3ac
Maxim Filippov <colixer@gmail.com>
2020-03-31 21:06:41 +0300

    Add logo

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -5,9 +5,23 @@
     </header>
     <main>
       <slot />
+      <footer class="logo-container">
+        <img src="~@/images/logo.png">
+      </footer>
     </main>
   </div>
 </template>
+
+<style lang="scss" scoped>
+footer {
+  & img {
+    width: 100px;
+  }
+  display: flex;
+  justify-content: center;
+  margin-top: 50px;
+}
+</style>
 
 <script>
 export default {
