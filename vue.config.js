f3367e3374ef0b25ff896fd35345f3c875789d7e
Maxim Filippov <colixer@gmail.com>
2023-03-06 01:25:48 +0000

    Allow all hosts

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,13 @@
 module.exports = {
   devServer: {
+    disableHostCheck: true,
+    allowedHosts: ["*"],
     proxy: {
-      '^/api': {
-        target: 'http://localhost:3000',
+      "^/api": {
+        target: "http://localhost:3000",
         ws: true,
-        changeOrigin: true
-      }
-    }
-  }
-}
+        changeOrigin: true,
+      },
+    },
+  },
+};
